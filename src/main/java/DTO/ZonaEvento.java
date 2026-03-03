2650335acc4282730b9dbfaf8f13f1bb2ad16d44
RuizC0D3 <ruizcode.exe@gmail.com>
2026-03-03 10:50:25 -0500

    feat: implementacion de logica de validacion RF-01 y estructura de DTOs

--- src/main/java/DTO/ZonaEvento.java
+++ src/main/java/DTO/ZonaEvento.java
@@ -1,4 +1,11 @@
 package DTO;
 
+import lombok.Data;
+
+@Data
 public class ZonaEvento {
+    private String nombreZona;
+    private double precio;
+    private int capacidadTotal;
+    private int boletasDisponibles;
 }
