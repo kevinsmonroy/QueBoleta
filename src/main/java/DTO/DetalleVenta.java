aaa203641b302187be447075c19626f83a2fb203
Kevin Monroy <kevins-monroyo@unilibre.edu.co>
2026-03-03 20:42:23 -0500

    Adicione los atributos y los getters y setters a la clase DetalleVenta

--- src/main/java/DTO/DetalleVenta.java
+++ src/main/java/DTO/DetalleVenta.java
@@ -1,4 +1,16 @@
 package DTO;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+
 public class DetalleVenta {
+
+    private int idDetalle;
+    private Localidad localidad;
+    private int cantidad;
+    private Double precioUnitario;
+
 }
