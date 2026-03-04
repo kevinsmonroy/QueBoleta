0562d2b2af1e46574bee34503e291c1d8713d21a
Kevin Monroy <kevins-monroyo@unilibre.edu.co>
2026-03-03 20:51:38 -0500

    Corregi los atributos y agregue getters y setters

--- src/main/java/DTO/Usuario.java
+++ src/main/java/DTO/Usuario.java
@@ -1,9 +1,12 @@
 package DTO;
 
-import lombok.Data;
-
-@Data
+import lombok.Getter;
+import lombok.Setter;
+@Getter
+@Setter
 public class Usuario {
+    private int numeroIdentificacion;
     private String nombre;
-    private String cedula;
+    private String correo;
+    private String telefono;
 }
