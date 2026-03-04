cf27321988b034a31c1c32885af0f135b9ecd490
Kevin Monroy <kevins-monroyo@unilibre.edu.co>
2026-03-03 21:03:37 -0500

    Adicione atributos y metodos getter y setter

--- src/main/java/DTO/BoletaGenerada.java
+++ src/main/java/DTO/BoletaGenerada.java
@@ -1,4 +1,13 @@
 package DTO;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+
 public class BoletaGenerada {
+    private int idBoleta;
+    private  String codigo;
+    private Localidad localidad;
 }
